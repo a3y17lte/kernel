88d93ab72396b1125a2996a0c96a839a223bf53b
Christopher N. Hesse <raymanfx@gmail.com>
2018-09-22 16:39:52 +0200

    gpio_keys: Report KEY_WAKEUP events on home button press

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -582,6 +582,9 @@ static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 		break;
 	case KEY_HOMEPAGE:
 		printk(KERN_INFO "[sec_input] HOME key is %s\n", !!state ? "pressed" : "released");
+		printk(KERN_INFO "GPIO-KEY: Sending KEY_WAKEUP (%s)\n", state ? "pressed" : "released");
+		input_report_key(input, KEY_WAKEUP, state > 0 ? 1 : 0);
+		input_sync(input);
 		break;
 	case KEY_VOLUMEUP:
 		printk(KERN_INFO "[sec_input] VolumeUp key is %s\n", !!state ? "pressed" : "released");
@@ -1067,6 +1070,8 @@ static int gpio_keys_probe(struct platform_device *pdev)
 	}
 
 	device_init_wakeup(&pdev->dev, wakeup);
+
+	set_bit(KEY_WAKEUP, input->keybit);
 
 	return 0;
 
